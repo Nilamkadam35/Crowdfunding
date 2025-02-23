92fd278e8de97b6d852622a3930b77ed88fef11f
Nilamkadam35 <99378041+Nilamkadam35@users.noreply.github.com>
2025-02-23 23:41:05 +0530

    Add files via upload

---
+++ Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Contact() {
+    return(
+        <div><h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam, tempora tempore delectus magni pariatur tenetur, placeat amet voluptatem suscipit facere, dignissimos aliquam. Praesentium nobis similique nemo consequuntur facilis dolores officia?</h2></div>
+    )
+}
+
+export default Contact
